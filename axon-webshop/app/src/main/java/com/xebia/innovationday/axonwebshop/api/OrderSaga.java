4e03c47040c392f485419be54acc79f766d7b605
MvdLinden <mvanderlinden@xebia.com>
2014-10-24 12:08:12 +0200

    Added reservation

--- axon-webshop/app/src/main/java/com/xebia/innovationday/axonwebshop/api/OrderSaga.java
+++ axon-webshop/app/src/main/java/com/xebia/innovationday/axonwebshop/api/OrderSaga.java
@@ -5,6 +5,8 @@ import org.axonframework.saga.annotation.AbstractAnnotatedSaga;
 import org.axonframework.saga.annotation.SagaEventHandler;
 import org.axonframework.saga.annotation.StartSaga;
 
+import java.util.UUID;
+
 public class OrderSaga extends AbstractAnnotatedSaga {
 
     private transient CommandGateway commmandGateway;
@@ -12,7 +14,8 @@ public class OrderSaga extends AbstractAnnotatedSaga {
     @StartSaga
     @SagaEventHandler(associationProperty = "orderId")
     public void handle(OrderCreatedEvent orderCreatedEvent) {
-        commmandGateway.send(new ReserveSeatCommand());
+
+        commmandGateway.send(new ReserveSeatCommand(UUID.randomUUID().toString()));
     }
 
     public void setCommmandGateway(CommandGateway commmandGateway) {
--- axon-webshop/app/src/main/java/com/xebia/innovationday/axonwebshop/api/ReserveSeatCommand.java
+++ axon-webshop/app/src/main/java/com/xebia/innovationday/axonwebshop/api/ReserveSeatCommand.java
@@ -2,4 +2,15 @@ package com.xebia.innovationday.axonwebshop.api;
 
 public class ReserveSeatCommand {
 
+
+    private final String reservationId;
+
+    public ReserveSeatCommand(String reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public String getReservationId() {
+        return reservationId;
+    }
+
 }
